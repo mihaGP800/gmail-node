93f9918ccd72dfc37bf637aaa4934a863de5be0d
mihaGP800 <mihagoroh@gmail.com>
2022-06-24 17:04:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const nodemailer = require("nodemailer");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3010
+
+app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/sendMessage', async (req, res) => {
+
+    const {name, email, message,} = req.body
+
+
+    let info = await transporter.sendMail({
+        from: 'Miha portfolio email', // sender address
+        to: "mihagoroh@gmail.com", // list of receivers
+        subject: "TEST EMAIL", // Subject line
+        //text: "Hello world?", // plain text body
+        html: `<b>Response from HR ${name}</b>
+                <p/>
+                message: ${message}
+                <p/>`,
+    });
+
+    res.send('ok')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    // service: "Gmail",
+    // port: 465,
+    // secure: true,
+    auth: {
+        user: 'portfolio.mihagoroh@gmail.com', // generated ethereal user
+        pass: 'opnugtkzcajkcpkk', // https://stackoverflow.com/questions/70606793/nodemailer-with-gmail-there-was-an-error-error-invalid-login-535-5-7-8-user
+    },
+});
